ec5d2d9b30cdf16e8709d11c7d8f0c67f4ca2f61
xueke <xuekepei@gmail.com>
2022-08-20 23:48:11 +0900

    :bug: Fix hex color

--- Sources/KKKit/Extensions/SwiftUI/Color+Extensions.swift
+++ Sources/KKKit/Extensions/SwiftUI/Color+Extensions.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 extension Color {
     public init(hex: Int, alpha: Double = 1) {
-        self.init(.sRGB, red: Double((hex >> 16) & 0xff), green: Double((hex >> 8) & 0xff), blue: Double(hex & 0xff), opacity: alpha)
+        self.init(.sRGB,
+                  red: Double((hex >> 16) & 0xff) / 255,
+                  green: Double((hex >> 08) & 0xff) / 255,
+                  blue: Double((hex >> 00) & 0xff) / 255,
+                  opacity: alpha
+        )
         
     }
     
